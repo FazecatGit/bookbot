512b607e1bebfd8058ff0edb849d0e03620390e1
Fazecat <peterisnot99@gmail.com>
2025-06-27 17:44:18 +0100

    A: First progress with main code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        frank_contents = f.read()
+    return frank_contents
+
+def word_counter(text):
+    words = text.split()
+    return len(words)
+    
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = word_counter(book_text)
+    print (f"{num_words} words found in the document")
+
+main()
